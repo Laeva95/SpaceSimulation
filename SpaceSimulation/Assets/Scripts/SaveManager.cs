c6f67fee94665d18540306804fcb39d41e08230c
Teinn <dnskrdl123@gmail.com>
2022-10-07 00:54:53 +0900

    Add CS, UI Object
    오프라인 진행, 보상 추가
    관련 UI 오브젝트 추가

--- SpaceSimulation/Assets/Scripts/SaveManager.cs
+++ SpaceSimulation/Assets/Scripts/SaveManager.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.IO;
+using System;
+using System.Text;
 
 [System.Serializable]
 public class SaveData
@@ -9,6 +12,7 @@ public class SaveData
     public ulong cp;
     public ulong dp;
     public ulong rp;
+    public string dataTime;
     public List<int> SCLevels = new List<int>();
     public List<int> PCLevels = new List<int>();
     public List<int> RelicLevels = new List<int>();
@@ -36,9 +40,16 @@ public class SaveManager : MonoBehaviour
     [SerializeField]
     RelicManager m_Relic;
 
+    [SerializeField]
+    Text m_OfflineRewardText;
+    [SerializeField]
+    GameObject m_OfflineRewardSet;
+
     private string SAVE_DATA_DIRECTORY;
 
     private string SAVE_FILENAME;
+
+    public long m_LastDateTime;
 
     private void Awake()
     {
@@ -58,8 +69,29 @@ public class SaveManager : MonoBehaviour
     private void Start()
     {
         LoadData();
-    }
 
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            StartCoroutine(GameQuit());
+        }
+    }
+    IEnumerator GameQuit()
+    {
+        yield return SaveDataCoroutine();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+    IEnumerator SaveDataCoroutine()
+    {
+        SaveData();
+        yield return new WaitForSeconds(0.5f);
+    }
+    #region 저장
     public void SaveData()
     {
         SaveData saveData = new SaveData();
@@ -79,14 +111,16 @@ public class SaveManager : MonoBehaviour
         {
             saveData.RelicLevels.Add(m_Relic.m_RelicLevels[i]);
         }
+        saveData.dataTime = SetLastPlayDate();
 
         string json = JsonUtility.ToJson(saveData);
 
-        File.WriteAllText(SAVE_DATA_DIRECTORY + SAVE_FILENAME, json);
+        SetLastPlayDate();
 
-        Debug.Log("--Save--");
-        Debug.Log(json);
+        File.WriteAllText(SAVE_DATA_DIRECTORY + SAVE_FILENAME, json);
     }
+    #endregion
+    #region 불러오기
     public void LoadData()
     {
         if (File.Exists(SAVE_DATA_DIRECTORY + SAVE_FILENAME))
@@ -120,6 +154,19 @@ public class SaveManager : MonoBehaviour
             }
 
             m_Resource.UpdateText();
+
+            m_LastDateTime = (int)DateTime.Now.Subtract(GetLastPlayDate(loadData.dataTime)).TotalSeconds;
+            Debug.Log(m_LastDateTime);
+            if (m_LastDateTime > 86400)
+            {
+                m_LastDateTime = 86400;
+            }
+
+            if (m_LastDateTime > 0)
+            {
+                StartCoroutine(TimeAfterLoadData());
+            }
+
             Debug.Log("--Load--");
             Debug.Log(loadJson);
         }
@@ -127,5 +174,57 @@ public class SaveManager : MonoBehaviour
         {
             Debug.Log("저장된 파일이 없습니다.");
         }
+    }
+    #endregion
+    IEnumerator TimeAfterLoadData()
+    {
+        yield return new WaitForSeconds(1f);
+        m_OfflineRewardSet.SetActive(true);
+        m_Resource.CreatePower += (ulong)m_LastDateTime * CPCal();
+        m_Resource.DivinityPower += (ulong)m_LastDateTime * DPCal();
+
+        m_OfflineRewardText.text = 
+            $"Gained {Utils.Caculation(CPCal() * (ulong)m_LastDateTime)} CP and {Utils.Caculation(DPCal() * (ulong)m_LastDateTime)} DP while {TimeCal()}.";
+    }
+    #region CP, DP 연산
+    ulong CPCal()
+    {
+        return m_Space.m_TotalCreatePower
+            * (ulong)(1 + m_Relic.m_Relics[0].m_Level + (m_Relic.m_Relics[4].m_Level * 2));
+    }
+    ulong DPCal()
+    {
+        return m_Planet.m_TotalDivinityPower
+            * (ulong)(1 + m_Relic.m_Relics[1].m_Level + (m_Relic.m_Relics[5].m_Level * 2));
+    }
+    string TimeCal()
+    {
+        string time = (m_LastDateTime / 3600).ToString() + "h" 
+            + ((m_LastDateTime % 3600) / 60).ToString() + "m" 
+            + ((m_LastDateTime % 3600) % 60).ToString() + "s";
+        return time;
+    }
+    public void OfflineSetFalse()
+    {
+        m_OfflineRewardSet.SetActive(false);
+    }
+    #endregion
+    string SetLastPlayDate()
+    {
+        return DateTime.Now.ToBinary().ToString();
+    }
+
+    DateTime GetLastPlayDate(string _dateTime)
+    {
+        if (_dateTime == null)
+            return DateTime.Now;
+
+        string binaryLastPlayDate = _dateTime;
+        long longLastPlayDate = Convert.ToInt64(binaryLastPlayDate);
+
+        Debug.Log(DateTime.FromBinary(longLastPlayDate));
+        Debug.Log(DateTime.Now);
+
+        return DateTime.FromBinary(longLastPlayDate);
     }
 }
